36084af562ba1cd029c255306b16d1984b913cd7
Oleksandr Vlasov <drumlife182@gmail.com>
2024-07-23 14:06:00 +0200

    Error handling

--- src/components/book-list-item/book-list-item.js
+++ src/components/book-list-item/book-list-item.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "./book-list-item.css";
-import { Link } from "react-router-dom";
 
 const BookListItem = ({ book }) => {
   const { title, author, price, coverImage } = book;
